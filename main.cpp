685a36423a7389da143ed906f201459490cb100e
ZacharyRenkema <zachary.renkema2005@gmail.com>
2024-06-06 21:36:16 -0500

    Created sever socket and adjusted error handling for dll initiation

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 // Library Includes
-# include <iostream>
+#include <iostream>
 // Winsock Includes
 #include <windows.h>
 #include <winsock2.h>
@@ -13,19 +13,41 @@
 # pragma comment(lib, "Ws2_32.lib")
 
 int main(int argc, char* argv[]) {
+	/* STEP 1:
+	 * The first step required is to initialize the DLL. Dll (Dynamic Link Library) is
+	 * a dynamic library that contains code and data to be used by more than one program
+	 * at a time.
+	 *
+	 * In this step, we need to initialize it with the need to create sockets that will
+	 * be used to communicate between the client and server. Additionally, the DLL ensures
+	 * that the network-related code is loaded and linked at runtime rather than at compile
+	 * time.
+	 */
 	SOCKET serverSocket, acceptSocket;
 	WSADATA wsaData;
-	WORD wVersionRequested = MAKEWORD(2,2);
+	// Formats word into proper format to pass into WSAStartup()
+	WORD wVersionRequested = MAKEWORD(2,2); // Version of Winsock
 	int wsaerr = WSAStartup(wVersionRequested, &wsaData);
 	int port = 59802;
-
-	if (wsaerr != 0) {
-		std::cout << "The Winsock dll is not found!" << std::endl;
-	}
-	else {
-		std::cout << "The Winsock dll found!" << std::endl;
+	// Check if DLL is found (WSAStartup returns 0 if passes)
+	if (wsaerr == 0) {
+		std::cout << "The Winsock dll is found!" << std::endl;
+		// Prints WSA status to terminal
 		std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
 	}
+	else {
+		std::cout << "The Winsock dll is not found!" << std::endl;
+	}
+	/* STEP 2:
+	 * In step 2 we refer to the protocol that will be used. Either UDP or TCP. In this
+	 * example TCP (Transmission Control Protocol) will be used. We must create the socket
+	 * using the socket function that is bound to a specific transport service provider
+	 *
+	 * Arguments:
+	 * 		af - The address family specification (AF_INET for UDP or TCP)
+	 * 		type - The type specification (TCP or UDP)
+	 * 		protocol - Protocol specification to be used (IPPROTO_TCP) for TCP
+	 */
 
 
 	return 0;
